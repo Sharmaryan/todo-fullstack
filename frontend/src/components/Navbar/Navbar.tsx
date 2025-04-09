94f6d8b9ffdd1c6d0371a85c3b2d42e8bdfbbb1c
Sharmaryan <panditaryan.as@gmail.com>
2025-04-09 13:39:54 +0530

    feat : navbar

---
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,29 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { TOKEN } from "../../utils/constants";
+
+export const Navbar = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const isAuthPage = pathname === "/signin" || pathname === "/signup";
+  return (
+    <nav className="bg-white shadow-md py-4 px-6 rounded-b-2xl">
+      <div className="max-w-7xl mx-auto flex justify-between items-center">
+        <h1 className="text-xl font-bold text-blue-600">MyTodoApp</h1>
+        {!isAuthPage &&(
+          <div className="space-x-4">
+            <button
+              className="text-gray-700 hover:text-blue-500 transition font-medium"
+              onClick={() => {
+                localStorage.removeItem(TOKEN);
+                navigate("/signin");
+              }}
+            >
+              Logout
+            </button>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
