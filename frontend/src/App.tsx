d1a683ceadc8b42b52019b82aa6264c65adb6415
Sharmaryan <panditaryan.as@gmail.com>
2025-04-09 13:40:10 +0530

    feat : applied private/protected routes in app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,19 +2,38 @@ import { BrowserRouter as Router, Route, Routes } from "react-router";
 import { Home } from "./pages/Home/Home";
 import { Login } from "./components/Login/Login";
 import { AuthType } from "./components/Login/Login.types";
+import { Navbar } from "./components/Navbar/Navbar";
+import { PrivateRoute } from "./components/PrivateRoute/PrivateRoute";
+import { PublicRoute } from "./components/PublicRoute/PublicRoute";
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
         <Route
           path="/signin"
-          element={<Login componentType={AuthType.SIGNIN} />}
+          element={
+            <PublicRoute>
+              <Login componentType={AuthType.SIGNIN} />
+            </PublicRoute>
+          }
         />
         <Route
           path="/signup"
-          element={<Login componentType={AuthType.SIGNUP} />}
+          element={
+            <PublicRoute>
+              <Login componentType={AuthType.SIGNUP} />
+            </PublicRoute>
+          }
         />
       </Routes>
     </Router>
