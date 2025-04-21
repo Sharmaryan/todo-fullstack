aef72b780ac72034f8e0c1a02247df1cf4ab69b4
Sharmaryan <panditaryan.as@gmail.com>
2025-04-21 10:57:12 +0530

    chore : updated env var name

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -10,7 +10,7 @@ const verifyToken = (req, res, next) => {
     const token = authHeader.split(' ')[1];
 
     try {
-        const decoded = jwt.verify(token, process.env.SECRET_TOKEN)
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
         req.user = decoded;
         next()
     }
