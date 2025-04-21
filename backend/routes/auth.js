bd3215913ccc703eb5692134c926d74ebfa4d196
Sharmaryan <panditaryan.as@gmail.com>
2025-04-21 10:57:30 +0530

    feat : refresh/auth token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,7 +8,7 @@ router.post('/signup', async (req, res) => {
     const password = req.body.password
     try {
         const user = await User.create({ email, password });
-        const token = await jwt.sign({ userId: user._id, email: user.email }, process.env.SECRET_TOKEN)
+        const token = await jwt.sign({ userId: user._id, email: user.email }, process.env.ACCESS_TOKEN_SECRET)
         res.status(200).json({ msg: 'User Registered', token })
     }
     catch (err) {
@@ -24,12 +24,38 @@ router.post('/signin', async (req, res) => {
         if (!user || !(await user.comparePasswords(password))) {
             return res.status(400).json({ error: 'Invalid credentials' });
         }
-        const token = await jwt.sign({ userId: user._id, email: user.email }, process.env.SECRET_TOKEN)
-        return res.status(200).json({ msg: 'User loggedin successfully', token })
+        const accessToken = await jwt.sign({ userId: user._id, email: user.email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15m' })
+        const refreshToken = await jwt.sign({ userId: user._id, email: user.email }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' })
+        res.cookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: 'strict',
+            path: '/api/auth/refresh-token',
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
+
+        return res.status(200).json({ msg: 'User loggedin successfully', accessToken })
     }
     catch (err) {
         return res.status(400).json({ err })
     }
+})
+
+router.post('/refresh-token', (req, res) => {
+    const refreshToken = req.cookies.refreshToken;
+
+    if (!refreshToken) {
+        return res.status(401).json({ err: 'Refresh token is missing' })
+    }
+
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) {
+            return res.status(403).json({ err: 'Invalid or expired refresh token' })
+        }
+        const accessToken = jwt.sign({ userId: user._id, email: user.email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15m' })
+        return res.status(200).json({ accessToken })
+
+    })
 })
 
 module.exports = router;
